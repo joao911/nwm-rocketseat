1545653e12a7b6ead12cc3c3fe47780f5f183345
joao911 <joaopaulolacerda911@gmail.com>
2020-08-07 16:25:41 -0300

    terceiro dia1/2

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader'
+import Warning from '../../assets/images/icons/warning.svg'
+import Input from '../../components/input';
+
+import './styles.css'
 
 function TeacherForm(){
     return(
         <div  id="page-teacher-form"className="container">
-        <PageHeader title="Que incrével que você quer dar aulas" />
+            <PageHeader
+                title="Que incrével que você quer dar aulas" 
+                description="O primeiro passo é preencher esse formulário de incrição"
+            />
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <Input name="Nome completo" label="name"/>
+                    <Input name="Avatar" label="avatar"/>
+                    <Input name="WhatsApp" label="whatsapp"/>
+                </fieldset>
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                    <Input name="Subject" label="Matéria"/>
+                    <Input name="cost" label="Custo da sua hora por aula"/>
+                </fieldset>
+                <footer>
+                    <p>
+                        <img src={Warning} alt="aviso importante"/>
+                        Importante <br/>
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">Salvar cadastro</button>
+                </footer>
+            </main>
         </div>
     )
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
-import './styles.css'
 import TeacherItem from '../../components/teacherItem';
+import Input from '../../components/input';
+
+import './styles.css'
 
 
 function TeacherList(){
@@ -9,23 +11,14 @@ function TeacherList(){
         <div  id="page-teacher-list"className="container">
             <PageHeader title="Estes são os professores disponíveis">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>
-                    </div>
+                   <Input name="subject" label="Materia"/>
+                   <Input name="week_day" label="Dia da seman"/>
+                   <Input type="time" name="time" label="hora"/>
                 </form>
             </PageHeader>
             <main>
                 <TeacherItem/>
-               
+        
             </main>
         </div >
     ) 
